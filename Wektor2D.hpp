718bd0881672f2ca39aba9a0b9e8ba76c14b3e9d
MichalPigon <michaal.pigon@gmail.com>
2024-10-22 20:16:41 +0200

    dobre

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,12 +1,12 @@
 class Wektor2D
 {
    public:
-      Wektor2D(){};
-      Wektor2D(double X, double Y){};
+      Wektor2D(): X(0), Y(0) {};
+      Wektor2D(double X, double Y): X(X), Y(Y){};
 
 
-   double getX(){ return X; }
-   double getY(){ return Y; }
+   double getX() const { return X; }
+   double getY() const { return Y; }
    
    void setX(double X){
       this->X = X;
@@ -17,8 +17,7 @@ class Wektor2D
 
    double norm()
    {
-      vec_norm = pow(X,2) + pow(Y,2);
-      vec_norm = pow(vec_norm,1/2);
+      vec_norm = std::sqrt(X * X + Y * Y);
       return vec_norm;
    }
 
@@ -30,11 +29,14 @@ class Wektor2D
    private:
       double X, Y, vec_norm;
 };
+double operator*(Wektor2D v1, Wektor2D v2)
+{
+   return v1.getX()*v2.getX() + v1.getY()*v2.getY();
+}
 Wektor2D operator+(Wektor2D v1, Wektor2D v2)
 {
-   return Wektor2D((v1.getX()+v2.getX()),(v1.getY()+v2.getY()));
-}
-Wektor2D operator*(Wektor2D v1, Wektor2D v2)
-{
-   return Wektor2D((v1.getX()*v2.getX()),(v1.getY()*v2.getY()));
+   Wektor2D v_sum;
+   v_sum.setX(v1.getX()+v2.getX());
+   v_sum.setY(v1.getY()+v2.getY());
+   return v_sum;
 }
